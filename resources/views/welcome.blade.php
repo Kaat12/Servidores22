959649aba9f600a0cb69bf589faa84d892cded46
Kaat12 <karentorrescon1@gmail.com>
2022-05-12 17:44:28 -0500

    Modificacion 12-05-2022

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                    <h1>Administraci&oacute;n de Servidores 2022 Karen Alvarez</h1>
+                   <h2> Instituto tecnologico de san luis potosi 2022 </h2>
                 </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
